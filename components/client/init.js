3ee4d14b6df2f5dc7c1b586d81579361bc725a68
Marc Solé Fonte <msolefonte@gmail.com>
2020-09-29 09:37:25 +0200

    Minor refactor

--- components/client/init.js
+++ components/client/init.js
@@ -1,74 +1,81 @@
 'use strict';
 
-const { 
-  app, 
-  BrowserWindow, 
-  dialog, 
-  session, 
-  shell 
+const {
+    app,
+    BrowserWindow,
+    dialog,
+    session
 } = require('electron');
 
-try {
 
-  if (app.requestSingleInstanceLock() !== true) app.quit()
+function main() {
+    try {
+        const manifest = require("./package.json");
 
-  const manifest = require("./package.json");
+        if (app.requestSingleInstanceLock() !== true) app.quit();
+        if (manifest.config["disable-gpu"]) app.disableHardwareAcceleration();
+        if (manifest.config.appid) app.setAppUserModelId(manifest.config.appid);
 
-  if (manifest.config["disable-gpu"]) app.disableHardwareAcceleration()
-  if (manifest.config.appid) app.setAppUserModelId(manifest.config.appid);
+        app.on('ready', function () {
 
-  app.on('ready', function(){
+            let options = manifest.config.window;
+            options.show = false;
+            options.webPreferences = {
+                devTools: manifest.config.debug || false,
+                nodeIntegration: true,
+                contextIsolation: false,
+                webviewTag: false
+            };
 
-    let options = manifest.config.window;
-    options.show = false;
-    options.webPreferences = {
-        devTools: manifest.config.debug || false,
-        nodeIntegration: true,
-        contextIsolation: false,
-        webviewTag: false
-    };
+            let win = new BrowserWindow(options);
 
-    let win = new BrowserWindow(options);
+            win.webContents.userAgent = manifest.config["user-agent"];
 
-    win.webContents.userAgent = manifest.config["user-agent"];
-    
-    session.defaultSession.webRequest.onBeforeSendHeaders((details, callback) => {
-      details.requestHeaders['User-Agent'] = manifest.config["user-agent"];
-      callback({ cancel: false, requestHeaders: details.requestHeaders });
-    });
-    
-    if (manifest.config.debug) {
-      win.webContents.openDevTools({mode: "undocked"});
-      win.isDev = true;
-      try {
-        const contextMenu = require('electron-context-menu')();
-      }catch(err){
-        dialog.showMessageBoxSync({type: "warning",title: "Debug Mode Failure", message: "Failed to initialize context menu.", detail: `${err}`});
-      }
+            session.defaultSession.webRequest.onBeforeSendHeaders((details, callback) => {
+                details.requestHeaders['User-Agent'] = manifest.config["user-agent"];
+                callback({cancel: false, requestHeaders: details.requestHeaders});
+            });
+
+            if (manifest.config.debug) {
+                win.webContents.openDevTools({mode: "undocked"});
+                win.isDev = true;
+                try {
+                    const contextMenu = require('electron-context-menu')();
+                } catch (err) {
+                    dialog.showMessageBoxSync({
+                        type: "warning",
+                        title: "Debug Mode Failure",
+                        message: "Failed to initialize context menu.",
+                        detail: `${err}`
+                    });
+                }
+            }
+
+            win.loadFile(manifest.config.window.view);
+
+            win.once('ready-to-show', () => {
+                win.show();
+                win.focus();
+            });
+
+            win.on('closed', () => {
+                win = null
+            });
+
+        })
+            .on('window-all-closed', function () {
+                app.quit();
+            })
+            .on('web-contents-created', (event, contents) => {
+                contents.on('new-window', (event, url) => {
+                    event.preventDefault();
+                });
+            });
+
+    } catch (err) {
+        dialog.showErrorBox("Critical Error", `Failed to initialize:\n${err}`);
+        app.quit();
     }
-    
-    win.loadFile(manifest.config.window.view);
-
-    win.once('ready-to-show', () => {
-      win.show();
-      win.focus();
-    });
-
-    win.on('closed', () => {
-      win = null
-    });
-
-  })
-  .on('window-all-closed', function() {
-      app.quit();
-  })
-  .on('web-contents-created', (event, contents) => {
-      contents.on('new-window', (event, url) => {
-        event.preventDefault();
-      });
-   });
-
-}catch(err) {
-  dialog.showErrorBox("Critical Error", `Failed to initialize:\n${err}`);
-  app.quit();
 }
+
+main();
